b74c2a2ce2016d0a5ba285a873ffb78b9485319a
harshith <MahankaliHarshith@gmail>
2022-11-23 11:55:22 +0530

    maven java project

---
+++ src/main/java/exam/exam1/App.java
@@ -0,0 +1,13 @@
+package exam.exam1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is Se lab internal 1 exam, the created project is Exam" );
+    }
+}
